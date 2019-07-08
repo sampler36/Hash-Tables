536f74075ac025e9d70d504399feabc5ffb517c3
Talent Antonio <antonio.talenty@gmail.com>
2019-07-08 17:39:34 +0100

    inserting

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -40,7 +40,8 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    hash_table.append((key, value))
+
 
 
 # '''
