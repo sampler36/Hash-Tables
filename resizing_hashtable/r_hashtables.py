100b4fa35bf1cac25b489e0ecbdc4a3b7caca416
Talent Antonio <antonio.talenty@gmail.com>
2019-07-09 10:20:17 +0100

    djb2

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -24,7 +24,10 @@ class HashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    hash = 5381
+    for x in string:
+        hash = (( hash << 5) + hash) + ord(x)
+    return hash % max
 
 
 # '''
